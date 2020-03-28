2039cb9ac75a6fe0bb9146cf3a0b9b46e7009ce5
Dubidu1212 <rz.ausserschwand@gmail.com>
2020-03-28 17:48:07 +0100

    Updates

---
+++ precache-manifest.ebeb6216d31ca5f3aeadfd2d7beb23f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af4a6416aea7187e18fdc5e3b1d41f92",
+    "url": "/ReactTest/index.html"
+  },
+  {
+    "revision": "8f94c794b88d3da05c76",
+    "url": "/ReactTest/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "a636f1521157af796859",
+    "url": "/ReactTest/static/js/2.6e9e15db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactTest/static/js/2.6e9e15db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f94c794b88d3da05c76",
+    "url": "/ReactTest/static/js/main.66ed939c.chunk.js"
+  },
+  {
+    "revision": "ffc495037f815afd97d1",
+    "url": "/ReactTest/static/js/runtime-main.61231b6f.js"
+  }
+]);
